08a165a8159867611f6b4e55e64d12af7f51cd42
masonpynt <masonpynt@icloud.com>
2023-07-26 15:44:37 +1000

    Many updates, keymaps unfinished

---
+++ lua/user/dbee.lua
@@ -0,0 +1,12 @@
+-- local config = {
+--   lazy = false,
+--   connections = {
+--     {
+--       name = "test_db",
+--       type = "sqlite",
+--       url = "home/mason/.config/lvim/test_db",
+--     },
+--   },
+-- }
+-- 
+-- require("dbee").setup(config)
