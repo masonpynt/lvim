9d250a416626d712c914e40763204d615e7e53c0
masonpynt <masonpynt@icloud.com>
2023-09-04 08:57:33 +1000

    plugins and options (needs fixing)

--- config.lua
+++ config.lua
@@ -1,6 +1,8 @@
 -- plugins --
-lvim.plugins = {
-{"lambdalisue/suda.vim"}
-}
+--lvim.plugins = {
+--{"lambdalisue/suda.vim"}
+--}
 -- w!! --
+reload "user.plugins"
+reload "user.options"
 vim.cmd [[noreabbrev <expr> w!! (getcmdtype() == ':' && getcmdline() == 'w!!') ? 'SudaWrite' : 'w!!']]
